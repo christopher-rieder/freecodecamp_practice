4fca89b88c00c5ff6947f7d7caf17c99ba724d5b
Christopher Rieder <christopher.rieder@gmail.com>
2018-11-25 00:04:04 -0300

    Intermediate Algorithm Scripting: DNA Pairing

--- javascript/javascript-second_certificate.js
+++ javascript/javascript-second_certificate.js
@@ -138,3 +138,26 @@ function myReplace (str, before, after) {
 console.log(
   myReplace('A quick brown fox Jumped over the lazy dog', 'jumped', 'leaped')
 );
+
+// Intermediate Algorithm Scripting: DNA Pairing
+
+/**
+ * @param {String} str A string
+ */
+function pairElement (str) {
+  return str.split('')
+    .map(char => {
+      switch (char) {
+        case 'G':
+          return ['G', 'C'];
+        case 'C':
+          return ['C', 'G'];
+        case 'A':
+          return ['A', 'T'];
+        case 'T':
+          return ['T', 'A'];
+      }
+    });
+}
+
+pairElement('GCG');
